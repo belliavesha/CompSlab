9ffacf1c535322b3cc7f2ec5d589f06089a7bba2
Vlad <belliavesha@gmail.com>
2017-09-18 14:21:11 +0300

    a couple of serious bugs fixed

--- cs.py
+++ cs.py
@@ -10,16 +10,35 @@ s,gn=pr(time0,0)
 from numpy import pi, exp, log, sqrt,sin,cos
 #from matplotlib.pyplot import *
 from scipy.special import kn
+#from scipy import empty 
 from numpy.polynomial.laguerre import laggauss
 from numpy.polynomial.legendre import leggauss
-#import numpy as np
+from numpy import linspace, empty
+import numpy as np
 #import matplotlib.pyplot as plt
 
 #precomputations :
-GaussLaguerre = laggauss(20) # laguerre-gauss sample points and weights
-GaussLegendre = leggauss(40) # legendre-gauss sample points and weights
-      # notice that if the numer of points is even then there is a zero angle or a cosine which equals to 1
-      # That will lead to Zero Division Error in this function (sometimes)
+NGamma=30 # number of \gamma points
+NAzimuth=40 # numbers of \phi points
+NEnergy = 10 # number of energy points
+NDirection = 4 # number of propagation angle cosines
+ # notice that if the numer of points is odd then there is a zero angle or a cosine which equals to 1
+ # That may lead to Zero Division Error in this functions 
+x_l, x_u = -5. , 1. # lower and upper bound of the energy span 
+logEnergySpan = linspace(x_l*log(10),x_u*log(10),num=NEnergy)
+IntEnergy = (exp(logEnergySpan), exp(-logEnergySpan),) # sample points and wights for computing the source function 
+IntGamma = laggauss(NGamma) # sample points and weights for computing thermal matrix
+IntAzimuth = leggauss(NAzimuth) # sample points and weights for computing azimuth-averaged matrix
+IntDirection = leggauss(NDirection) # sample points and weights for computing intencity integral
+      
+
+def setTemperature(T):
+      K = kn(2,1./T) # second modified Bessel function of reversed dimensionless temperature       
+      return (T, K) # Theta is the dimensionless electron gas temperature (Theta = k * T_e / m_e c^2)
+
+(Theta, K2Y) = setTemperature(.1)
+
+
 
 
 s,gn=pr(s,gn)
@@ -53,14 +72,15 @@ def Compton_redistribution_m(x1,x2,mu,gamma):
       u = a2-a1  #(x1+x2)*(2.*gamma+x2-x1)/(a1+a2) # the formulas probably give always the same numbers 
       
       q = x1*x2*(1.-mu)
-      Q = sqrt( x1*x1 + x2*x2 - 2.*x1*x2*mu )
+      Q = sqrt( x1*x1 + x2*x2 - 2.*x1*x2*mu ) # sqrt( (x1-x2)**2 +2.*q ) # the formula gives the same
       gammaStar = (x1-x2+Q*sqrt( 1. + 2./q ) )/2.
 #      print r,v,u,q,Q,gammaStar
 
       if gamma < gammaStar : 
             print 'w'
             return  (0.,0.,0.,0.)
-      else:
+      else: 
+
             Ra = u*( u*u - Q*Q )*( u*u + 5.*v )/2./q/q/v/v/v + u*Q*Q/q/q/v/v
             Rb = 2./Q + u/v*( 1. - 2./q )
             Rc = u/q/v*( ( u*u - Q*Q )/r/q - 2.)
@@ -70,29 +90,31 @@ def Compton_redistribution_m(x1,x2,mu,gamma):
             RI = Ra + Rc
             RU = Rd + 2.*Rc
             RQ = RU + Ra
-            #if gamma- gammaStar < 2 : print ';;;',gamma- gammaStar
+            
+            #if gamma- gammaStar < 2 : previousnt ';;;',gamma- gammaStar
 
             #print r,v,u,q,Q,gammaStar,Ra,Rb,Rc,R
-
-      
+            #print x1,x2,mu,gamma,R,RI,RQ,RU
+            
             return (R,RI,RQ,RU)
+            #the return values of this functon seem to be all right
 
 
 
 
-def Maxwell_r(Theta,gamma):
+def Maxwell_r(gamma):
       """The normalized relativistic Maxwellian distribution
       the density of particles in the dimensionless momentum volume (4 \pi z^2 dz) is nomalized to unity
       Theta is the dimensionless electron gas temperature (Theta = k * T_e / m_e c^2)
       gamma is electron energy in units of the electron rest mass
       The fuction returns the momentum dencity value ( f(\gamma) )
       """
-      # kn is a modified Bessel function
-      r = 1./4./pi/Theta*exp(-gamma/Theta)/kn(2,1./Theta)
+      r = .25/pi/Theta*exp(-gamma/Theta)/K2Y
       return r
 
-def Compton_redistribution(x1,x2,mu,Theta): 
+def Compton_redistribution(x1,x2,mu): 
       """thermal Compton redistribution matrix (integrated with electron distribution function)
+      And the distribution is maxwellian
       x1 and x2 - photon energies in units of electron rest mass ( h \nu / m_e c^2 ) 
       mu - cosine of a scattering angle 
       This fuctions returns (R,RI,RQ,RU)
@@ -104,14 +126,14 @@ def Compton_redistribution(x1,x2,mu,Theta):
       q = x1*x2*(1.-mu)
       Q = sqrt( x1*x1 + x2*x2 - 2.*x1*x2*mu )
       gammaStar = (x1-x2+Q*sqrt( 1. + 2./q ) )/2.
-      C=3./8.*Maxwell_r(Theta,gammaStar)
-      
+      C=3./8.*Theta*Maxwell_r(gammaStar)
+            
      
-      point, weight = GaussLaguerre 
-      NL = range(len(point)) # number of laguerre-gauss sample points
+      point, weight = IntGamma 
+      NL = range(NGamma) # list of indeces
       
       summands = map(lambda i : map(lambda x: C*x*weight[i],\
-           Compton_redistribution_m(x1,x2,mu,point[i]+gammaStar)),NL)
+           Compton_redistribution_m(x1,x2,mu,Theta*point[i]+gammaStar)),NL)
       R = [sum([summands[j][i] for j in NL]) for i in range(4)] # summing to obtain the integrals
       
       # # the next 5 lines do the same thing as previous three but faster for some reason
@@ -125,7 +147,7 @@ def Compton_redistribution(x1,x2,mu,Theta):
       return tuple(R)
       
 
-def Compton_redistribution_aa(x1,mu1,x2,mu2,Theta):
+def Compton_redistribution_aa(x1,mu1,x2,mu2):
       """azimuth-avereged Compton redistribution matrix 
       for computing of electron scattering source function 
       x1 and x2 are photon energies in units of electron rest mass ( h \nu / m_e c^2 ) 
@@ -134,51 +156,70 @@ def Compton_redistribution_aa(x1,mu1,x2,mu2,Theta):
       We need only 2x2 matrix in the upper left corner of the general matrix,
       becouse U and V components on the Stokes vector are zero in this case.
       """
-      
+      # this function gives not the same result as its old fortran ancestor for some reason
+      # the difference is a factor depending of x1 and x2, but the relations between different elements are alright
+
+
       mur1 = sqrt( 1. - mu1*mu1 ) # sinuses of the angles 
       mur2 = sqrt( 1. - mu2*mu2 ) # what the R stands for? I dunno.
       
-      point, weight = GaussLegendre
-      NL = range(len(point)) # number of laguerre-gauss sample points
+      point, weight = IntAzimuth
+      NL = range(NAzimuth) # indeces list
       
       az_c=cos(pi*point)  # list of azimuth cosines
       az_s=sin(pi*point)  # list of azimuth sinuses
       sc_c=mu1*mu2-mur1*mur2*az_c # list of scattering angles cosines
-      cos2chi1 = map(lambda c : 2.*(mu2-mu1*c)**2/mur1/mur1/(1.-c*c) - 1. , sc_c)
-      cos2chi2 = map(lambda c : 2.*(mu1-mu2*c)**2/mur2/mur2/(1.-c*c) - 1. , sc_c)
-      sin2chi1 = map(lambda c, s : -(mu1-mu2*c)*mur2/mur1/(1.-c*c) , sc_c , az_s)
-      sin2chi2 = map(lambda c, s :  (mu1-mu2*c)*mur1/mur2/(1.-c*c) , sc_c , az_s)
+      cos2chi1 = map(lambda c : 2.*(mu2-mu1*c)**2/mur1/mur1/(1.-c*c) - 1. , sc_c) # list[ cos( 2 \chi_1 ) ]
+      cos2chi2 = map(lambda c : 2.*(mu1-mu2*c)**2/mur2/mur2/(1.-c*c) - 1. , sc_c) # list[ cos( 2 \chi_2 ) ]
+      sin2chiP = map(lambda c, s : -4.*((mu1-mu2*c)*s/(1.-c*c))**2 , sc_c , az_s) # list[ sin( 2 \chi_1 )*sin( 2 \chi_2 ) ]
+      # sin2chi1 = map(lambda c, s : -(mu1-mu2*c)*mur2*s/mur1/(1.-c*c) , sc_c , az_s) # list[ sin( 2 \chi_1 ) ]
+      # sin2chi2 = map(lambda c, s :  (mu1-mu2*c)*mur1*s/mur2/(1.-c*c) , sc_c , az_s) # list[ sin( 2 \chi_2 ) ]
       # cos2chi12 = map(lambda c,s : ((mu2-mu1*c)**2-mur1*mur1*mur2*mur2*s*s)/mur1/mur1/(1.-c*c) , sc_c,az_s)
       # print max(map(lambda a,b:abs(a-b)/(a+b), cos2chi12,cos2chi1)) # comparing two formulas. It turns out they're the same
       
       R=[[0.,0.],[0.,0.]]
       for i in NL:
-            (C,I,Q,U)=Compton_redistribution(x1,x2,sc_c[i],Theta)
+            (C,I,Q,U)=Compton_redistribution(x1,x2,sc_c[i])
             R[0][0]+=C*pi*weight[i]
             R[0][1]+=I*pi*cos2chi2[i]*weight[i]
             R[1][0]+=I*pi*cos2chi1[i]*weight[i]
-            R[1][1]+=(Q*pi*cos2chi1[i]*cos2chi2[i]+U*pi*sin2chi1[i]*sin2chi2[i])*weight[i]
+            R[1][1]+=pi*(Q*cos2chi1[i]*cos2chi2[i]-U*sin2chiP[i])*weight[i]        
       
       
+      print x1,x2,mu1,mu2,R
       return R
+
+
+# s,gn=pr(s,gn)
+
+# x1,x2=2.,3.
+# for NL in range(1,10):
+#       mu1,mu2=.1,.2
+#       print Compton_redistribution_aa(x1,mu1,x2,mu2,.1/NL)
 
 
 
 
 s,gn=pr(s,gn)
-x1,x2=2.,3.
-for NL in range(1,10):
-      mu1,mu2=.1,.2
-      print Compton_redistribution_aa(x1,mu1,x2,mu2,.1/NL)
+
+Theta, K2Y = setTemperature(.1)
+print Compton_redistribution_aa(1e-2,-.5,1e-1 ,.5)
+RedistributionMatrix = empty( (NEnergy,NDirection,NEnergy,NDirection) )
+# for e in range(NEnergy):
+#  for d in range(NDirection):
+#   for e1 in range(NEnergy):
+#    for d1 in range(NDirection):
+#       a=Compton_redistribution_aa(
+#         IntEnergy[0][e],\
+#         IntDirection[0][d],\
+#         IntEnergy[0][e1],\
+#         IntDirection[0][d1])
+
+
 
 s,gn=pr(s,gn)
 
 print 'Total time : ', s-time0
-# ps, garbage=  laggauss(20) 
-# plot(range(20),log(ps))
-
-# def pr(x):
-#       print x,sn(x)
 
 # ars=map(lambda a: 1e2*2.**(-a),(range(20)))
 # print map(sn,ars)
